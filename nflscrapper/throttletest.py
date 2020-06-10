d980c10650a273f6c53883b3d2e1ac2a6dee58ef
Derek Adair <d@derekadair.com>
2020-06-10 16:48:33 +0000

    Basic structure solidified for scraper

--- nflscrapper/throttletest.py
+++ nflscrapper/throttletest.py
@@ -1,10 +1,11 @@
 import re
+import json
 import sys
 import logging
 import time
 import random
 import asyncio
-import asyncio
+import os
 
 import aiofiles
 import aiohttp
@@ -47,14 +48,61 @@ async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
     html = await resp.text()
     return html
 
+async def parse(url: str, session: ClientSession, **kwargs) -> set:
+    games = [
+   {
+    "away": "BUF",
+    "day": 9,
+    "eid": "2009080950",
+    "gamekey": "54723",
+    "home": "TEN",
+    "month": 8,
+    "season_type": "PRE",
+    "time": "8:00",
+    "wday": "Sun",
+    "week": 0,
+    "year": 2009
+   }
+   ]
+    try:
+        html = await fetch_html(url=url, session=session, **kwargs)
+    except (
+        aiohttp.ClientError,
+        aiohttp.http_exceptions.HttpProcessingError,
+    ) as e:
+        logger.error(
+            "aiohttp exception for %s [%s]: %s",
+            url,
+            getattr(e, "status", None),
+            getattr(e, "message", None),
+        )
+        return games
+    except Exception as e:
+        logger.exception(
+            "Non-aiohttp exception occured:  %s", getattr(e, "__dict__", {})
+        )
+        return games
+    else:
+        logger.info("Found %d links for %s", len(games), url)
+        return games
+
+async def write_results(data: dict) -> None:
+    outfile = os.path.join(os.path.dirname(__file__), 'schedule.json')
+    json_str = json.dumps(data)
+    async with aiofiles.open(outfile, "w+") as f:
+        await f.write(json_str)
+        logger.info("Wrote results for source URLs")
+
 async def worker(throttler, session, urls):
+    games = list()
+    logger.info("Worker fetching {} urls".format(len(urls)))
     for url in urls:
         async with throttler:
-            print(time.time(), 'Worker: Bang!')
-            await fetch_html(url, session)
+            games += await parse(url, session)
+    await write_results(games)
 
 async def main():
-    throttler = Throttler(rate_limit=3, period=2)
+    throttler = Throttler(rate_limit=3, period=.1)
     async with ClientSession() as session:
         urls = build_urls(2020)
         tasks = [loop.create_task(worker(throttler, session, urls))]
